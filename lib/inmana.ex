0d48d3b7eaee3d78f91ce7335c97432c96f2582d
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2022-10-25 22:35:32 -0300

    feat: post supply route

--- lib/inmana.ex
+++ lib/inmana.ex
@@ -1,5 +1,8 @@
 defmodule Inmana do
-  alias Inmana.Restaurants.Create
+  alias Inmana.Restaurants.Create, as: CreateRestaurant
+  alias Inmana.Supplies.Create, as: CreateSupply
 
-  defdelegate create_restaurant(params), to: Create, as: :call
+  defdelegate create_restaurant(params), to: CreateRestaurant, as: :call
+
+  defdelegate create_supply(params), to: CreateSupply, as: :call
 end
